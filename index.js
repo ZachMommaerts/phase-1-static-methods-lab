334d638d7fc51d01a17122541d3102800a886f0f
Zach <zmommaerts@gmail.com>
2022-01-26 13:01:16 -0600

    stuck

--- index.js
+++ index.js
@@ -5,11 +5,19 @@ class Formatter {
   }
 
   static sanitize(string) {
-    return string.replace(/[^-,'A-Za-z0-9]+/g, "");
+    return string.replace( /[^A-Za-z0-9 '-]/g, '' )
   }
 
-  static titleize() {
+  static titleize(string) {
+    const words = string.split(' ');
 
+    for (let i = 0; i < words.length; i++) {
+      if (words[i] !== 'a' || words[i] !== 'an' || words[i] !== 'but' || words[i] !== 'of' || words[i] !== 'and' || words[i] !== 'for' || words[i] !== 'at' || words[i] !== 'by' || words[i] !== 'from' || words[i] !== 'the' ) {
+        words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+      } 
+    }
+
+    return words.join(' ');
   }
 
 }
